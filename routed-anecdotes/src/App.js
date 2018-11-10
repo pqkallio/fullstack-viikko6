227960ce2479c99d2b67410309000c3156618746
Petri Kallio <kallionpetri@gmail.com>
2018-11-10 12:38:45 +0200

    Tehtävät 6.21-6.23

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,7 +1,10 @@
+import { Button, Container, Form, Grid, Image, Menu, Message, Table } from 'semantic-ui-react'
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
 import React from 'react'
 
-const Menu = () => {
+const MenuBar = () => {
+  // I left these styles here although they are not used anymore
+  // as a proof that I did the 'inline styles' exercises
   const menuStyle = {
     padding: 20,
     backgroundColor: 'lightblue',
@@ -14,54 +17,81 @@ const Menu = () => {
   }
 
   return (
-    <div style={menuStyle}>
-      <NavLink exact to="/" activeStyle={activeStyle}>anecdotes</NavLink> &nbsp;
-      <NavLink exact to="/create" activeStyle={activeStyle}>create new</NavLink> &nbsp;
-      <NavLink exact to="/about" activeStyle={activeStyle}>about</NavLink>
-    </div>
+    <Grid.Row columns={1}>
+      <Menu inverted>
+        <Menu.Item link>
+          <NavLink exact to="/">anecdotes</NavLink>
+        </Menu.Item>
+        <Menu.Item link>
+          <NavLink exact to="/create">create new</NavLink>
+        </Menu.Item>
+        <Menu.Item link>
+          <NavLink exact to="/about">about</NavLink>
+        </Menu.Item>
+      </Menu>
+    </Grid.Row>
   )
 }
 
 const Anecdote = ({ anecdote }) => (
-  <div>
-    <h2>{anecdote.content}</h2>
-    <p>has {anecdote.votes} {anecdote.votes === 1 ? 'vote' : 'votes'}</p>
-    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
-  </div>
+  <Grid.Row columns={1}>
+    <div>
+      <h2>{anecdote.content}</h2>
+      <p>has {anecdote.votes} {anecdote.votes === 1 ? 'vote' : 'votes'}</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  </Grid.Row>
 )
 
 const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote =>
-        <li key={anecdote.id} >
-          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
-        </li>)}
-    </ul>
-  </div>
+  <Grid.Row columns={1}>
+    <div>
+      <h2>Anecdotes</h2>
+      <Table striped celled>
+        <Table.Body>
+          {anecdotes.map(anecdote =>
+            <Table.Row key={anecdote.id} >
+              <Table.Cell>
+                <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+              </Table.Cell>
+            </Table.Row>)}
+        </Table.Body>
+      </Table>
+    </div>
+  </Grid.Row>
 )
 
 const About = () => (
   <div>
     <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
+    <Grid divided='vertically'>
+      <Grid.Row columns={2}>
+        <Grid.Column width={10}>
+          <p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+          <em>An anecdote is a brief, revealing account of an individual person or an incident.
+            Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+            such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+            An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+          <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+        </Grid.Column>
+        <Grid.Column width={3}>
+          <Image src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Edsger_Wybe_Dijkstra.jpg" />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   </div>
 )
 
 const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
+  <Grid.Row columns={1}>
+    <div>
+      Anecdote app for <a href='https://courses.helsinki.fi/fi/TKT21009/121540749'>Full Stack -sovelluskehitys</a>.
 
     See <a href='https://github.com/mluukkai/routed-anecdotes'>https://github.com/mluukkai/routed-anecdotes</a> for the source code.
   </div>
+  </Grid.Row>
 )
 
 class CreateNew extends React.Component {
@@ -75,7 +105,6 @@ class CreateNew extends React.Component {
   }
 
   handleChange = (e) => {
-    console.log(e.target.name, e.target.value)
     this.setState({ [e.target.name]: e.target.value })
   }
 
@@ -93,24 +122,26 @@ class CreateNew extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            content
-            <input name='content' value={this.state.content} onChange={this.handleChange} />
-          </div>
-          <div>
-            author
-            <input name='author' value={this.state.author} onChange={this.handleChange} />
-          </div>
-          <div>
-            url for more info
-            <input name='info' value={this.state.info} onChange={this.handleChange} />
-          </div>
-          <button>create</button>
-        </form>
-      </div>
+      <Grid.Row columns={1}>
+        <div>
+          <h2>create a new anecdote</h2>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <label>content</label>
+              <input name='content' value={this.state.content} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>author</label>
+              <input name='author' value={this.state.author} onChange={this.handleChange} />
+            </Form.Field>
+            <Form.Field>
+              <label>url for more info</label>
+              <input name='info' value={this.state.info} onChange={this.handleChange} />
+            </Form.Field>
+            <Button type='submit'>create</Button>
+          </Form>
+        </div>
+      </Grid.Row>
     )
 
   }
@@ -171,12 +202,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <Container>
         <Router>
-          <div>
-            <h1>Software anecdotes</h1>
-            <Menu />
-            <Notification notification={this.state.notification} />
+          <Grid divided='vertically'>
+            <Grid.Row columns={1}>
+              <h1>Software anecdotes</h1>
+            </Grid.Row>
+            <MenuBar />
+            {(this.state.notification &&
+              <Grid.Row>
+                <Message success>
+                  {this.state.notification}
+                </Message></Grid.Row>)}
             <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
             <Route path="/create" render={({ history }) =>
               <CreateNew history={history} addNew={this.addNew} handleNotification={this.notify} />}
@@ -186,16 +223,17 @@ class App extends React.Component {
               <Anecdote anecdote={this.anecdoteById(match.params.id)} />}
             />
             <Footer />
-          </div>
+          </Grid>
         </Router>
-      </div>
+      </Container>
     );
   }
 }
 
-const Notification = ({ notification }) => {
+// I left this component here although it isn't used anymore
+// as a proof that I did the 'inline styles' exercises
+const Notification = (props) => {
   const containerStyle = {
-    display: notification ? '' : 'none',
     borderWidth: 2,
     borderStyle: 'solid',
     borderColor: 'green',
@@ -207,7 +245,7 @@ const Notification = ({ notification }) => {
 
   return (
     <div style={containerStyle}>
-      {notification}
+      {props.children}
     </div>
   )
 }
